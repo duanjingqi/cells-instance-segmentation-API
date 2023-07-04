77bd09023f11f10d1456b11ad1ca7d3c059fb860
duanjingqi <duanjingqi@gmail.com>
2023-07-04 10:40:40 -0700

    Add docker_setup.sh

---
+++ docker_setup.sh
@@ -0,0 +1,24 @@
+# Remove all conflicting packages
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc
+do
+
+    apt-get remove $pkg
+
+done
+
+# Set up apt repository
+apt-get update
+apt-get install ca-certificates curl gnupg
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine 
+apt-get update 
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
